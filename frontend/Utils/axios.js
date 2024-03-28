c61405247f66c8dd448eedf98ede26e58bd2230a
Bhavesh Mali <80244760+bhavesh14319@users.noreply.github.com>
2024-03-28 20:50:29 +0530

    Update axios.js

--- frontend/Utils/axios.js
+++ frontend/Utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : "http://localhost:4000/api/v1",
-    // baseURL: "https://mysterious-pig-buckle.cyclic.app/api/v1",
+    // baseURL : "http://localhost:4000/api/v1",
+    baseURL: "https://mysterious-pig-buckle.cyclic.app/api/v1",
     headers: {
         'Access-Control-Allow-Origin' : '*',
         'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
