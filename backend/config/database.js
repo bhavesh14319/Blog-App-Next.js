345da2566a201ef7d787ab89fdd1793cc7b2e5ca
Bhavesh <bhavesh.mali052002@gmail.com>
2025-03-15 00:09:28 +0530

    changed db connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,41 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const connectDatabase = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI).then((con)=>{
-            console.log('Connected to database: ',con.connection.host);
-        }).catch((e)=>{
-            console.log(e);
-        })
-    }catch(e){
-        console.log(e)
-    }
-}
+// const connectDatabase = async ()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO_URI).then((con)=>{
+//             console.log('Connected to database: ',con.connection.host);
+//         }).catch((e)=>{
+//             console.log(e);
+//         })
+//     }catch(e){
+//         console.log(e)
+//     }
+// }
 
-module.exports=connectDatabase;
+// module.exports=connectDatabase;
+
+const mongoose = require("mongoose");
+
+let isConnected = false; // Track connection state
+
+const connectDatabase = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = db.connections[0].readyState;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDatabase;
