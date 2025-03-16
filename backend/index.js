48c66c5c2a541958b2c25f082fb5034545b363c6
Bhavesh <bhavesh.mali052002@gmail.com>
2025-03-16 19:39:30 +0530

    deploy

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const app = require('./app');
+
+
+const connectDatabase = require('./config/database');
+const cloudinary = require("cloudinary");
+
+
+
+cloudinary.config({
+  cloud_name: "dxx38ccfx",
+  api_key: "549216885663435",
+  api_secret: "DHSgyPTnPS59EawXBdgAXQ4J2Vw"
+});
+
+connectDatabase().then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log('server running on port ', process.env.PORT);
+    })
+})
+
+
+connectDatabase();
+
