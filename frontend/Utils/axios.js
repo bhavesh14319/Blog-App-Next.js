0cff760b34c74b6f2da861c5f20f4690c14f545e
Bhavesh Mali <80244760+bhavesh14319@users.noreply.github.com>
2025-03-16 19:51:27 +0530

    Update axios.js

--- frontend/Utils/axios.js
+++ frontend/Utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL : "http://localhost:4000/api/v1",
-    baseURL: "https://blog-app-next-js-one.vercel.app/api/v1",
+    baseURL: "https://blog-app-next-js-black.vercel.app/api/v1",
     headers: {
         'Access-Control-Allow-Origin' : '*',
         'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
